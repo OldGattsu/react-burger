364d852152bf2b1affed4ed5e27451f2c9241bdc
Artyom Krasavin <krasavin-00@mail.ru>
2021-08-19 15:09:14 +0300

    upd

--- src/components/burger-ingredients/burger-ingredients.js
+++ src/components/burger-ingredients/burger-ingredients.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import styles from './burger-ingredients.module.css';
 
+import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
+
 export default function BurgerIngredients() {
+  const [currentTab, setCurrentTab] = React.useState('buns')
   return (
     <section className={styles.burgerIngredients}>
-
+      <h1 className="text text_type_main-large">Соберите бургер</h1>
+      <div style={{ display: 'flex' }}>
+        <Tab value="buns" active={currentTab === 'buns'} onClick={setCurrentTab}>
+          Булки
+        </Tab>
+        <Tab value="sauces" active={currentTab === 'sauces'} onClick={setCurrentTab}>
+          Соусы
+        </Tab>
+        <Tab value="toppings" active={currentTab === 'toppings'} onClick={setCurrentTab}>
+          Начинки
+        </Tab>
+      </div>
     </section>
   )
 }
