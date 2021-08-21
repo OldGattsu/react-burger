c21e21bc0d391a408b3a996232490b6d7ed826c3
Artyom <krasavin-00@mail.ru>
2021-08-21 19:29:53 +0300

    upd

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import styles from './app.module.css';
+
 import AppHeader from '../app-header/app-header';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients';
 import BurgerConstructor from '../burger-constructor/burger-constructor';
+import Loader from 'react-loader-spinner';
 
 const ingredientsAPi = 'https://norma.nomoreparties.space/api/ingredients ';
 
@@ -23,11 +25,23 @@ export default function App() {
     <div>
       <AppHeader />
       <main className={styles.main}>
-        {ingredients.length !==0 && (
-          <>
-            <BurgerIngredients ingredients={ingredients} />
-            <BurgerConstructor ingredients={ingredients} />
-          </> )}
+        {ingredients.length ===0
+          ? (
+            <Loader
+              className="loader"
+              type="Puff"
+              color="#00BFFF"
+              height={70}
+              width={70}
+            />
+          )
+          : (
+            <>
+              <BurgerIngredients ingredients={ingredients} />
+              <BurgerConstructor ingredients={ingredients} />
+            </>
+          )
+        }
       </main>
     </div>
   );
