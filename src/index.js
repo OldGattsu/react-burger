3f9082744ccedbe43c65d60b74accd4bb09451c4
Artyom Krasavin <krasavin-00@mail.ru>
2021-09-06 20:00:58 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import reportWebVitals from './reportWebVitals';
 import './index.css';
 import App from './components/app/app';
-import reportWebVitals from './reportWebVitals';
+
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit'
+import thunk from 'redux-thunk'
+import rootReducer from './store/reducers'
+
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: [thunk],
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('react')
 );
