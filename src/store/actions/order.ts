4c88475944a6b55d88e34000b52db4515ee50a45
Artyom Krasavin <krasavin-00@mail.ru>
2021-10-11 19:55:28 +0300

    upd

--- src/store/actions/order.ts
+++ src/store/actions/order.ts
@@ -1,12 +1,16 @@
 import { createAction, createAsyncThunk } from '@reduxjs/toolkit'
 import { sendRequest, ORDERS } from '../../utils/api-helper'
+import { IIngredient } from '../../types/ingredient'
 
-export const getOrderId = createAsyncThunk('order/getOrderId', async (ids: number[]) =>
-  sendRequest(ORDERS, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(ids),
-  })
+
+export const getOrderId = createAsyncThunk<IIngredient, number[]>(
+  'order/getOrderId',
+  async (ids: number[]) =>
+    sendRequest(ORDERS, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(ids),
+    })
 )
 
 export const clearOrderId = createAction('order/clearOrder')
